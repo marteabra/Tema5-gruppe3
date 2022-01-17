6fb90ab8d7511ea6243f28150fec923fb1a2c090
tse018 <60438717+tse018@users.noreply.github.com>
2022-01-17 19:26:35 +0100

    Added Header buttons, navbar + searchfield

--- project/src/components/Header.vue
+++ project/src/components/Header.vue
@@ -9,7 +9,7 @@
             <i class="fas fa-bars"></i>
          </button>
 
-         <ul v-if="this.menuVisible" class="header__open">
+         <ul v-if="menuVisible" class="header__open">
             <li v-for="page in navigationPages" class="header__item">
                {{ page.title }}
             </li>
@@ -39,7 +39,7 @@
             <i class="fas fa-search"></i>
          </button>
 
-         <div v-if="this.searchVisible" class="header__container">
+         <div v-if="searchVisible" class="header__container">
             <input type="text" placeholder="search for articles, books, etc." name="searchbar" class="header__input" /> 
          </div>
       </nav>
@@ -74,88 +74,104 @@
 </script>
 
 <style>
-   @media screen and (max-width: 800px) {
-      .header__mobile {
-         display: none;
-      }
+@media screen and (max-width: 800px) {
+   .header__mobile {
+      display: none;
    }
+}
 
-   .header {
-      position: fixed;
-      width: 100%;
-      height: 10%;
-      z-index: 100;
-   }
+.header {
+   position: fixed;
+   width: 100%;
+   height: 10%;
+   z-index: 100;   
+}
 
-   .header__navbar {
-      display: flex;
-      justify-content: space-between;
-   }
+.header__navbar {
+   display: flex;
+   justify-content: space-between;
+}
 
-   .header__logo {
-      position: absolute;
-      margin: 5px 0 0 10px;
-      text-decoration: none;
-      color: black;
-   }
+.header__logo {
+   position: absolute;
+   margin: 5px 0 0 10px;
+   text-decoration: none;
+   color: var(--primary);
+}
 
-   .header__menu {
-      position: absolute;
-      right: 0;
-      margin: 5px 10px 0 0;
-   }
+.header__menu {
+   position: absolute;
+   right: 0;
+   margin: 5px 10px 0 0;
+}
 
-   .header__open {
-      width: 100%;
-      height: 220px;
-      background-color: rgba(235, 228, 228, 0.911);
-   }
+.header__open {
+   width: 100%;
+   height: 220px;
+   background-color: var(--secondary);
+}
 
-   .header__item {
-      display: flex;
-      justify-content: center;
-   }
+.header__item {
+   display: flex;
+   justify-content: center;
+}
 
-   .header__item--about {
-      position: absolute;
-      top: 0;
-      left: 65%;
-   }
+.header__item--about {
+   position: absolute;
+   top: 0;
+   left: 65%;
+}
 
-   .header__item--instagram {
-      position: absolute;
-      top: 60%;
-      left: 65%;
-   }
+.header__item--instagram {
+   position: absolute;
+   top: 60%;
+   left: 65%;
+}
 
-   .header__item--twitter {
-      position: absolute;
-      top: 85%;
-      left: 65%;
-   }
+.header__item--twitter {
+   position: absolute;
+   top: 85%;
+   left: 65%;
+}
 
-   .header__item--mail {
-      position: absolute;
-      top: 155%;
-      left: 65%;
-   }
+.header__item--mail {
+   position: absolute;
+   top: 155%;
+   left: 65%;
+}
 
-   .header__search {
-      position: absolute;
-      right: 0;
-      margin: 5px 50px 0 0;
-   }
+.header__search {
+   position: absolute;
+   right: 0;
+   margin: 5px 50px 0 0;
+}
 
-   .fa-search {
-      cursor: pointer;
-      color: gray;
-      font-size: 30px;
-   }
+.header__container {
+   width: 100%;
+   height: 50px;
+   background-color: var(--secondary);
+}
 
-   .fa-bars {
-      cursor: pointer;
-      color: gray;
-      font-size: 30px;
-   }
+.header__input {
+   position: absolute;
+   top: 10px;
+   right: 200px;
+   width: 500px;
+   border: none;
+   border-bottom-style: groove;
+   background-color: transparent;
+}
+
+.fa-search {
+   cursor: pointer;
+   color: var(--primary);
+   font-size: 30px;
+}
+
+.fa-bars {
+   cursor: pointer;
+   color: var(--primary);
+   font-size: 30px;
+}
 
 </style>
