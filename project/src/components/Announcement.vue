ea753fc55dfa53f130d50e83486768c10f0f061c
Julie Viktoria <julieviktoria1@hotmail.com>
2022-01-19 11:24:24 +0100

    🐛  setup

---
+++ project/src/components/Announcement.vue
@@ -0,0 +1,40 @@
+<template>
+   <div class="announcement__box"> 
+      <div class="announcement__text">
+
+      </div>
+   </div>
+</template>
+
+<script>
+   export default {
+      data() {
+         return {
+    
+         }
+      }
+   }
+</script>
+
+<style>
+   @media screen and (max-width: 600px) {
+     
+   }
+
+   .announcement__box {
+      position: absolute;
+      left: 0%;
+      right: 0%;
+      top: 0%;
+      bottom: 0%;
+      width: 100%;
+      background: #ADFF00;
+   }
+
+   .announcement__text {
+      font-family: JetBrains Mono;
+      font-weight: normal;
+      font-size: 30px;
+      line-height: 40px;
+   }
+</style>
