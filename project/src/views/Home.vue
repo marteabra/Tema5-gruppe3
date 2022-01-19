b1b3319fe64b1f50561221bb75fc2c7a36c76637
marteabra <marte.abrahams1@hotmail.com>
2022-01-19 14:19:38 +0100

    header footer slideshow implemented in home.vue

--- project/src/views/Home.vue
+++ project/src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <Header />
+  <Slideshow />
   <Footer />
 </template>
 
 <script>
 import Header from "../components/Header.vue";
 import Footer from "../components/Footer.vue";
+import Slideshow from "../components/Slideshow.vue";
 
 export default {
   components: {
     Header,
+    Slideshow,
     Footer,
   },
 };
