456d8b651118d9986d3d698ac8b9b68658d6a25b
IrynaOstapenko <52035358+IrynaOstapenko@users.noreply.github.com>
2022-01-19 14:04:30 +0100

    commit

--- project/src/views/Home.vue
+++ project/src/views/Home.vue
@@ -1,13 +1,16 @@
 <template>
    <Header />
+   <Slideshow />
 </template>
 
 <script>
    import Header from '../components/Header.vue';
+   import Slideshow from '../components/Slideshow.vue';
 
    export default {
       components: {
          Header,
+         Slideshow,
       },
    }
 </script>
