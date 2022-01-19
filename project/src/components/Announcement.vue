b652fff9c9e45aaff9b26e378a27ccf0e52335eb
Julie Viktoria <julieviktoria1@hotmail.com>
2022-01-19 18:32:11 +0100

    ✨ announcements text & design

--- project/src/components/Announcement.vue
+++ project/src/components/Announcement.vue
@@ -1,7 +1,7 @@
 <template>
    <div class="announcement__box"> 
       <div class="announcement__text">
-
+         OUT NOW! Issue #123 “Dialogues on Recursive Colonialisms, Speculative Computation, and the Techno-social”
       </div>
    </div>
 </template>
@@ -10,7 +10,19 @@
    export default {
       data() {
          return {
-    
+            slide: 0,
+            sliding: null,
+            direction: 'slide-left',
+            show: true
+         }
+      },
+
+      methods: {
+         onSlideStart(slide) {
+            this.sliding = true
+         },
+         onSlideEnd(slide) {
+            this.sliding = false
          }
       }
    }
@@ -18,23 +30,27 @@
 
 <style>
    @media screen and (max-width: 600px) {
-     
+      
    }
 
    .announcement__box {
-      position: absolute;
+      position: relative;
       left: 0%;
       right: 0%;
-      top: 0%;
+      top: 50px;
       bottom: 0%;
       width: 100%;
+      height: 2.5em;
       background: #ADFF00;
    }
 
    .announcement__text {
       font-family: JetBrains Mono;
       font-weight: normal;
-      font-size: 30px;
+      font-size: 20px;
       line-height: 40px;
+      height: 2.5em;
+      animation: slide 5s infinite backwards;
+      overflow: hidden;
    }
 </style>
--- project/src/views/Home.vue
+++ project/src/views/Home.vue
@@ -1,13 +1,16 @@
 <template>
    <Header />
+   <Announcement />
 </template>
 
 <script>
    import Header from '../components/Header.vue';
+   import Announcement from '../components/Announcement.vue';
 
    export default {
       components: {
          Header,
+         Announcement,
       },
    }
 </script>
