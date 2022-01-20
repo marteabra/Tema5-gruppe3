a4052c034820740e437c99dfc52295312035ad9b
tse018 <60438717+tse018@users.noreply.github.com>
2022-01-20 12:10:56 +0100

    Articles data got from store

---
+++ project/src/components/MainPageArticles.vue
@@ -0,0 +1,34 @@
+<template>
+   <div class="articles">
+      <div class="articles__new">
+         Latest
+      </div>
+
+      <div v-for="article in getArticles" :key="article.title" class="articles__container">
+         <div class="articles__title">
+            {{ article.title }}
+         </div>
+
+         <div class="articles__lead">
+            {{ article.lead }}
+         </div>
+
+         <div class="articles__image">
+            {{ article.preview.image }}
+         </div>
+      </div>
+   </div>
+</template>
+
+<script>
+   export default {
+      computed: {
+         getArticles() {
+            return this.$store.getters.getArticles;
+         },
+      },
+   };
+</script>
+
+<style>
+</style>
