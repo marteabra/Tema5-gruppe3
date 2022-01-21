88de678686540ccb34ca98f28c25f2fd23e37f98
marteabra <marte.abrahams1@hotmail.com>
2022-01-21 13:29:07 +0100

    bug fix LAST

---
+++ project/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <Header />
+  <MainPageArticles />
+  <Footer />
+</template>
+<script>
+import Header from "../components/Header.vue";
+import MainPageArticles from "../components/MainPageArticles.vue";
+import Footer from "../components/Footer.vue";
+import Slideshow from "../components/Slideshow.vue";
+export default {
+  components: {
+    Header,
+    MainPageArticles,
+    Slideshow,
+    Footer,
+  },
+};
+</script>
+<style>
+</style>
