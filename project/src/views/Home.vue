1b2dc866cb7465c84611c9ffbf349837cd8b06d7
Julie Viktoria <julieviktoria1@hotmail.com>
2022-01-21 13:37:31 +0100

    🎨 adding announcement to Home

--- project/src/views/Home.vue
+++ project/src/views/Home.vue
@@ -1,16 +1,20 @@
 <template>
   <Header />
+  <Announcement />
   <MainPageArticles />
   <Footer />
 </template>
 <script>
 import Header from "../components/Header.vue";
+import Announcement from "../components/Announcement.vue";
 import MainPageArticles from "../components/MainPageArticles.vue";
 import Footer from "../components/Footer.vue";
 import Slideshow from "../components/Slideshow.vue";
+
 export default {
   components: {
     Header,
+    Announcement,
     MainPageArticles,
     Slideshow,
     Footer,
