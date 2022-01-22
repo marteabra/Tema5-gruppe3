2cb8c12d1428ceed702cd125061be8c76fd2898f
tse018 <60438717+tse018@users.noreply.github.com>
2022-01-22 19:53:03 +0100

    small adjustment to route params

--- project/src/routes.js
+++ project/src/routes.js
@@ -2,6 +2,6 @@ import Home       from './views/Home.vue';
 import Articles   from './views/Articles.vue';
 
 export default [
-      { name: 'article',      path: '/:article_id',     component: Articles },
+      { name: 'article',      path: '/article/:article_id',     component: Articles },
       { name: 'home',         path: '/',                component: Home},
 ]
