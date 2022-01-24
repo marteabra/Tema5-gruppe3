8298e500b57c5d499bbb0f0f35fd4a638090974f
Julie Viktoria <julieviktoria1@hotmail.com>
2022-01-24 11:47:17 +0100

    🚀 setup about-us

---
+++ project/src/components/AboutUs.vue
@@ -0,0 +1,36 @@
+<template>
+    <div v-for="page in pageContent" class="h1">
+        {{ page.content.title }}
+
+    </div>
+
+        <div>
+            {{ body }}
+        </div>
+
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                body: [''],
+            };
+        },
+
+        computed: {
+            pageContent() {
+                return this.$store.getters.getPageContent;
+            },
+        },
+
+        mounted() {
+            this.body = this.$store.state.pages.content.body;
+            console.log(this.store);
+        },
+    }
+</script>
+
+<style>
+
+</style>
---
+++ project/src/views/Page.vue
@@ -0,0 +1,33 @@
+<template>
+<Header />
+<AboutUs />
+<Footer />
+</template>
+
+
+<script>
+import Header from "../components/Header.vue";
+import AboutUs from "../components/AboutUs.vue";
+import Footer from "../components/Footer.vue";
+
+export default {
+  components: {
+    Header,
+    AboutUs,
+    Footer
+  },
+};
+</script>
+
+
+<style>
+    Header {
+        z-index: 1;
+        top: 0;
+    }
+
+    Footer {
+        position: relative;
+        bottom: 0;
+    }
+</style>
