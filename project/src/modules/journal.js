0f4ec4371a1028f4c926d91fb8a9bf50de00392f
marteabra <marte.abrahams1@hotmail.com>
2022-01-28 15:58:39 +0100

    last netlify image check

--- project/src/modules/journal.js
+++ project/src/modules/journal.js
@@ -2,20 +2,20 @@ export default {
    state() {
       return {
          slides: [
-            { 
-               title: 'image by April Bey', 
+            {
+               title: 'image by April Bey',
                caption: '“And My Flames Stay Till You Get Out My Way” Museum für Moderne Kunst April Bey',
-               file: 'project/public/images/journal_1.jpeg'
+               file: './images/journal_1.jpeg'
             },
             {
-               title: 'image by Kenny Fries', 
-               caption: '“Crip Time” Vienna Art Week Kenny Fries', 
-               file: 'project/public/images/journal_2.jpeg'
+               title: 'image by Kenny Fries',
+               caption: '“Crip Time” Vienna Art Week Kenny Fries',
+               file: './images/journal_2.jpeg'
             },
-            { 
-               title: 'image by Novuyo Moyo', 
-               caption: '“Losing Control” Alte Pinakothek Novuyo Moyo', 
-               file: 'project/public/images/journal_3.jpeg'
+            {
+               title: 'image by Novuyo Moyo',
+               caption: '“Losing Control” Alte Pinakothek Novuyo Moyo',
+               file: './images/journal_3.jpeg'
             },
          ],
       }
