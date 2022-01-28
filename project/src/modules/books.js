15d36afb0765a49c6ed1aa788a2d449066291ada
marteabra <marte.abrahams1@hotmail.com>
2022-01-28 15:58:26 +0100

    netlify checked

--- project/src/modules/books.js
+++ project/src/modules/books.js
@@ -5,32 +5,32 @@ export default {
             {
                title: 'Radicalizing Care',
                writers: 'Birgit Bosold, Lena Fritsch, Vera Hofmann, Elke Krasny, Sophie Lingg',
-               cover: 'project/public/images/book_1.png'
+               cover: './images/book_1.png'
             },
             {
                title: 'Curating the Complex & The Open Strike',
                writers: 'TERRY SMITH',
-               cover: 'project/public/images/book_2.png'
+               cover: './images/book_2.png'
             },
             {
                title: 'Amazonia',
                writers: 'KATERYNA BOTANOVA, QUINN LATIMER',
-               cover: 'project/public/images/book_3.png'
+               cover: './images/book_3.png'
             },
             {
                title: 'Parapolitics',
                writers: 'ANSELM FRANKE, NIDA GHOUSE, PAZ GUEVARA, ANTONIA MAJACA (EDS.)',
-               cover: 'project/public/images/book_4.png'
+               cover: './images/book_4.png'
             },
             {
                title: 'Visual Cultures as Time Travel',
                writers: 'HENRIETTE GUNKEL, AYESHA HAMEED',
-               cover: 'project/public/images/book_5.png'
+               cover: './images/book_5.png'
             },
             {
                title: 'Dear Navigator',
                writers: 'HU FANG',
-               cover: 'project/public/images/book_6.jpg'
+               cover: './images/book_6.jpg'
             },
          ],
       };
